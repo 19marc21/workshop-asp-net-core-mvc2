637582032765948b8022caf60f4aaa33305ee369
malbertoaraujo.19@gmail.com <100146181+19marc21@users.noreply.github.com>
2022-03-14 07:52:46 -0300

    criando class-depatment-salesrecord-seller

--- SalesWebMvc2/Models/Department.cs
+++ SalesWebMvc2/Models/Department.cs
@@ -1,10 +1,20 @@
 ﻿
-
+using System.Collections.Generic;
 namespace SalesWebMvc2.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string  Nome { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+        }
+        public Department(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
     }
 }
